00d8c3bd402b4d792f70b0bec0a22fd84c97860b
vicky0304200 <vigneshwaran.b2020cse@sece.ac.in>
2025-09-16 16:55:51 +0530

    added changes

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -38,7 +38,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'login',
+    'backend.login',
     'rest_framework',
    
 ]
